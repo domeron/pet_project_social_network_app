201569e138f7ee655128821aabf93d6c6e1773df
Dauren Yergaliyev <domeron02@gmail.com>
2023-10-16 17:05:00 +0500

    added signin and signup pages to blazor client project

---
+++ SocialNetworkAppLibrary/DTO/SignInDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetworkAppLibrary.DTO;
+public class SignInDTO
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required]
+    [MaxLength(100), MinLength(2)]
+    public string Password { get; set; }
+}
---
+++ SocialNetworkAppLibrary/DTO/SignUpDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetworkAppLibrary.DTO;
+public class SignUpDTO
+{
+    [Required]
+    [MaxLength(100)]
+    [MinLength(2)]
+    public string FirstName { get; set; }
+    [Required]
+    [MaxLength(100)]
+    [MinLength(2)]
+    public string LastName { get; set; }
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required]
+    public string Password { get; set; }
+
+}
